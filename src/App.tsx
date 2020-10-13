1e22c57c0ef09ae3871ce11598f386130ef08e9f
iwasa-kosui <contact@ebiebievidence.com>
2020-10-13 23:15:53 +0900

    feat: add via

--- src/App.tsx
+++ src/App.tsx
@@ -109,7 +109,7 @@ function App() {
       <p>です</p>
       <br />
       <input type="button" value="生成" onClick={onClick} />
-      <TwitterShareButton url={window.location.href} title={title}>
+      <TwitterShareButton url={window.location.href} title={title} via="ebiebievidence">
           <TwitterIcon size={32} round />
       </TwitterShareButton>
     </div>
